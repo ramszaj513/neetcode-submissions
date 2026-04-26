d0eb4aa47b62a56031c91be210661e4fe3a1f517
Jakub Ramsza <jakubr513@gmail.com>
2026-04-26 23:43:58 +0200

    Add: merge-k-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-2.cs
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution{
+    public ListNode MergeKLists(ListNode[] lists) {
+        if(lists.Length == 0) return null;
+        if(lists.Length == 1) return lists[0];
+
+        int k = 1;
+        while (k < lists.Length) {
+            for (int i = 0; i + k < lists.Length; i += k * 2) {
+                lists[i] = MergeTwoLists(lists[i], lists[i + k]);
+            }
+            k *= 2;
+        }
+
+        return lists[0];
+
+        // ListNode dummy = new ListNode(0);
+        // ListNode tail = dummy;
+
+        // while (true) {
+        //     ListNode minNode = null;
+        //     int minIdx = -1;
+        //     int minVal = int.MaxValue;
+
+        //     for (int i = 0; i < lists.Length; i++) {
+        //         if (lists[i] != null && lists[i].val < minVal) {
+        //             minVal = lists[i].val;
+        //             minNode = lists[i];
+        //             minIdx = i;
+        //         }
+        //     }
+
+        //     if (minIdx == -1) break;
+
+        //     tail.next = lists[minIdx];
+        //     tail = tail.next;
+        //     lists[minIdx] = lists[minIdx].next;
+        // }
+        // return dummy.next;
+    }
+
+    private ListNode MergeTwoLists(ListNode l1, ListNode l2){
+        if(l1 == null && l2 == null) return null;
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while(l1 != null && l2 != null){
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+
+            tail = tail.next;
+        }
+
+        tail.next = (l1 == null) ? l2 : l1;
+        return dummy.next;
+    }
+}
+
