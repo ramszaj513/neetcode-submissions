bff345974c7848613face9b018c94f71305167f7
Jakub Ramsza <jakubr513@gmail.com>
2026-04-27 12:39:20 +0200

    Add: invert-a-binary-tree - submission-4

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-4.cs
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+
+    // recursive DFS
+    // public TreeNode InvertTree(TreeNode root) {
+    //     if(root == null) return null;
+
+    //     TreeNode rootleft = root.left;
+    //     root.left = InvertTree(root.right);
+    //     root.right = InvertTree(rootleft);
+
+    //     return root;
+    // }
+
+    // iterative DFS
+    // public TreeNode InvertTree(TreeNode root){
+    //     if(root == null) return null;
+    //     Stack<TreeNode> stack = new Stack<TreeNode>();
+    //     stack.Push(root);
+    //     while(stack.Count > 0){
+    //         TreeNode node = stack.Pop();
+    //         (node.left, node.right) = (node.right, node.left);
+    //         if(node.left != null) stack.Push(node.left);
+    //         if(node.right != null) stack.Push(node.right);
+    //     }
+
+    //     return root;
+    // }
+
+    // BFS
+    public TreeNode InvertTree(TreeNode root){
+        if(root == null) return null;
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        while(queue.Count > 0){
+            TreeNode node = queue.Dequeue();
+            (node.left, node.right) = (node.right, node.left);
+            if(node.left != null) queue.Enqueue(node.left);
+            if(node.right != null) queue.Enqueue(node.right);
+        }
+
+        return root;
+    }
+}
