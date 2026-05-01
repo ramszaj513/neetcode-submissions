65d5e1b82f4b99188a0ab30d716a971f59e3c4cf
Jakub Ramsza <jakubr513@gmail.com>
2026-05-01 12:11:12 +0200

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public List<List<int>> Subsets(int[] nums) {
+        // we choose to add a number
+        // resursivly call it on the next number with the current list of numbers
+        // we choose not to add a number
+        // resursivly call on the next number with the current list of numbers
+        // time complexity: n*2^n
+        // space complexity: 2^n
+
+        List<List<int>> res = new();
+
+        void Backtrack(int index, List<int> subset){
+            if(index > nums.Length - 1){
+                res.Add(new List<int>(subset));
+                return;
+            }
+
+            // add the current number to the subset
+            subset.Add(nums[index]);
+            Backtrack(index + 1, subset);
+
+            // dont add the current number to the subset
+            subset.Remove(nums[index]);
+            Backtrack(index + 1, subset);
+        }
+
+
+        Backtrack(0,new List<int>());
+        return res;
+    }
+}
