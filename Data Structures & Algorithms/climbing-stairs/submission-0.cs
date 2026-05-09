49d73a853e5c5f5acc08bf706a53cc43c6058d2d
Jakub Ramsza <jakubr513@gmail.com>
2026-05-09 17:32:35 +0200

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if(n == 1) return 1;
+
+        int[] steps = new int[n];
+        steps[0] = 1;
+        steps[1] = 2;
+
+        for(int i = 2; i < n; i++){
+            steps[i] = steps[i-1] + steps[i-2];
+        }
+
+        return steps[n - 1];    
+    }
+}
