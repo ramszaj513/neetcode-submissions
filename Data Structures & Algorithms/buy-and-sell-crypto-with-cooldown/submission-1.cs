fbdb05a5a35abac71204fbd835414d97653f9691
Jakub Ramsza <jakubr513@gmail.com>
2026-05-10 17:34:09 +0200

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        // two dimentional array 
+        // - dp[i,0] represents the max value we can get when we are in the buying state
+        // - dp[i,1] represents the max value we can get when we are in the selling state
+
+        // each time we get the best outcome of both states
+        // when in buying:
+        // - buy (so we check the best result from the selling table one day in the future minus our price)
+        // - wait (we check the best result when we are in buying one day in the future)
+
+        // when in selling
+        // - sell (so we check the best result from the buying two days in the future current price or just out price if we cant but in the future)
+        // - wait (we check what is the result of selling tomorrow
+
+        int n = prices.Length;
+        int[,] dp = new int[n + 1, 2];
+
+        for (int i = n - 1; i >= 0; i--) {
+            // we can buy
+            int buy = dp[i + 1, 0] - prices[i];
+            int cooldown = dp[i + 1, 1];
+            dp[i, 1] = Math.Max(buy, cooldown);
+
+            // we can sell
+            int sell = (i + 2 < n) ? dp[i + 2, 1] + prices[i] : prices[i];
+            cooldown = dp[i + 1, 0];
+            dp[i, 0] = Math.Max(sell, cooldown);
+        }
+
+        return dp[0, 1];
+    }
+}
