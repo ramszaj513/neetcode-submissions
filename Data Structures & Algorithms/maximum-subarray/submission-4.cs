869a723573c559f555a631dd3b2e1bd276bbaa17
Jakub Ramsza <jakubr513@gmail.com>
2026-05-10 18:23:09 +0200

    Add: maximum-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-subarray/submission-4.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        // sliding window approach
+
+        int currSum = nums[0];
+        int max = nums[0];
+
+        for(int r = 1; r < nums.Length; r++){
+            if(nums[r] > currSum && currSum < 0){
+                currSum = nums[r];
+            } else{
+                currSum += nums[r];
+            }
+
+            max = Math.Max(max,currSum);
+        }    
+
+        return max;
+    }
+}
